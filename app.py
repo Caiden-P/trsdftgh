1cb576fb8fc0f0ea4e33e182b32389517099e1e3
Caiden <119334408+Caiden-P@users.noreply.github.com>
2024-10-29 21:33:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def root():
     return "We've got lift-off."
+
+@app.route('/', subdomain='dashboard')
+def dashboard():
+    return "Welcome to the Dashboard!"
+    
