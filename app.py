669db260146ed48ec801f97de28a3789436c962d
Caiden <119334408+Caiden-P@users.noreply.github.com>
2024-10-29 21:11:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
-# app.py
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-# Set the server name for subdomain handling
-app.config['SERVER_NAME'] = 'trsdftgh.onrender.com'  # Change this to your actual domain
 
-@app.route('/', defaults={'subdomain': None})
-@app.route('/', subdomain='www')  # For the main site (www.domain.com)
-def home(subdomain):
-    return "Welcome to the main site!"
+@app.route("/")
+def root():
+    return "We've got lift-off."
 
-@app.route('/', subdomain='dashboard')  # Access dashboard via dashboard.domain.com
-def dashboard():
-    return "Welcome to the Dashboard!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+app.run()
